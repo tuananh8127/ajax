86ef425b9fecb68116f83a1931ed03df240eda8c
mr anh <votuananh8127@gmail.com>
2016-08-17 12:50:36 +0700

    index

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
-<?php
-echo "hello ajax"; 
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<div id="result"></div>
+
+<script>
+
+var html = new XMLHttpRequest;
+
+html.open('GET', 'data.php', false);
+
+html.send();
+document.getElementById('result').innerHTML = html.responseText;
+
+
+</script>
+
+
+</body>
+</html>
